893647030d8090d24fa634ef2801cb4e645f1b11
Gagan Hothi <G@1337.local>
2016-08-18 21:53:24 -0700

    initial commit - broken

---
+++ app/scripts/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name coinioApp
+ * @description
+ * # coinioApp
+ *
+ * Main module of the application.
+ */
+angular.module('coinioApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase',
+    'firebase.ref',
+    'firebase.auth'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name coinioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the coinioApp
+ */
+angular.module('coinioApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

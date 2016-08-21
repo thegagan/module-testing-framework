38b92addc83f7053e078a465ce1dc698e9539da4
Gagan Hothi <G@1337.local>
2016-08-21 04:03:41 -0700

    added ngclass color selection based on pricing. multiple atomic styles. cleaned up and added effect to voting buttons.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,24 +16,36 @@ angular.module('coinioApp').controller('MainCtrl', function ($scope) {
 
 	$scope.pCard = [
 		{
-			user: "betaUser99",
-			userPts: 4.56,
-			coin: "BTC",
-			trend: "long",
-			buy: .00325,
-			sell: .00452,
-			stop: .00306,
-			score: 3.65
+			user: 'betaUser99',
+			userId: '1',
+			userPts: '4.56',
+			coin: 'BTC',
+			trend: 'long',
+			buy: '.00325',
+			sell: '.00452',
+			stop: '.00306',
+			score: '3.65'
 		}, {
-			user: "gaznox",
-			userPts: -1.46,
-			coin: "ETC",
-			trend: "short",
-			buy: .00325,
-			sell: .00452,
-			stop: .00306,
-			score: 3.65
+			user: 'gaznox',
+			userId: '2',
+			userPts: '-1.46',
+			coin: 'ETC',
+			trend: 'short',
+			buy: '.00325',
+			sell: '.00452',
+			stop: '.00306',
+			score: '-3.65'
 		}
 	];
+
+	$scope.colorize = function(card) {
+		if (card.score > 0) {
+			return "green";
+		}
+		if (card.score < 0) {
+			return "red";
+		}
+
+	};
 
 });

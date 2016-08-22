fe3ee84c43e3550db53b57481167b9992dbf473c
gaznox <gaznox@gmail.com>
2016-08-22 15:44:32 -0700

    added configfb.js

---
+++ app/scripts/configfb.js
@@ -0,0 +1,8 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBp_z86AizKYQwNTaAyPWBI9vrnYzei6ok",
+  authDomain: "coin-io.firebaseapp.com",
+  databaseURL: "https://coin-io.firebaseio.com",
+  storageBucket: "coin-io.appspot.com",
+};
+firebase.initializeApp(config);

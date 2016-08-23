15d4e45f1eab755c8020f81bdcc2137ec1114609
Gagan Hothi <G@1337.local>
2016-08-23 12:31:13 -0700

    new favicon, new fabric texture for left nav.

---
+++ app/scripts/services/pcard.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name coinioApp.pcard
+ * @description
+ * # pcard
+ * Service in the coinioApp.
+ */
+angular.module('coinioApp')
+  .service('pcard', function (pNew) {
+    
+		var pNew = {
+			"userName": "",
+			"userId": "",
+			"coin": "",
+			"position": "",
+			"open": "",
+			"close": "",
+			"stop": ""
+		};
+
+  });
